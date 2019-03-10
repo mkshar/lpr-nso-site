f17125acff876db5d766b14137128292bdbb5ac2
Mikhail Sharoglazov <sharoglazov@sibext.com>
2019-03-11 01:40:44 +0700

    Updated site app, added badges constructor.

--- apps/lpr_nso_site/src/lpr_nso_site_app.erl
+++ apps/lpr_nso_site/src/lpr_nso_site_app.erl
@@ -7,6 +7,21 @@
 
 -include( "lager" ).
 
+get_env( Name, Default_value ) ->
+
+	case os:getenv( Name ) of
+		false -> Default_value;
+		Value -> Value
+	end
+.
+
+to_integer( Value ) ->
+
+	if is_list( Value ) -> list_to_integer( Value ); true -> Value end
+.
+
+% localhost:8080
+
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
@@ -17,78 +32,59 @@ start( Start_type, Start_args) ->
 
 	Dispatch = cowboy_router:compile
 	([
-		{ '_', [
-			{ "/", cowboy_static, { priv_file, lpr_nso_site, "index.html" } },
-			{ "/members/[...]", cowboy_static, { priv_dir, lpr_nso_site, "members", [{mimetypes, {<<"text">>, <<"html">>, []}}] } },
-			{ "/media/[...]",   cowboy_static, { priv_dir, lpr_nso_site, "media" } },
-			{ "/apps/[...]",   cowboy_static, { priv_dir, lpr_nso_site, "apps" } },
-			{ "/fonts/[...]",   cowboy_static, { priv_dir, lpr_nso_site, "fonts" } },
-			{ "/logo/[...]",   cowboy_static, { priv_dir, lpr_nso_site, "logo" } },
-			{ "/css/[...]",   cowboy_static, { priv_dir, lpr_nso_site, "css" } },
-			{ "/library/[...]", cowboy_static, { priv_dir, lpr_nso_site, "library" } },
-%			{ "/google_homeapp_webhook",    google_homeapp_webhook,    [] },
-			{ "/websocket", lpr_nso_site_handler, [] },
-			{ "/static/[...]", cowboy_static, { priv_dir, lpr_nso_site, "static" } }
-		]}
+		{
+			'_',
+			[
+				{ "/",              cowboy_static, { priv_file, lpr_nso_site, "index.html" } },
+				%{ "/members/[...]", cowboy_static, { priv_dir, lpr_nso_site, "members", [{mimetypes, {<<"text">>, <<"html">>, []}}] } },
+				{ "/media/[...]",   cowboy_static, { priv_dir,  lpr_nso_site, "media"      } },
+				{ "/apps/[...]",    cowboy_static, { priv_dir,  lpr_nso_site, "apps"       } },
+				{ "/fonts/[...]",   cowboy_static, { priv_dir,  lpr_nso_site, "fonts"      } },
+				{ "/logo/[...]",    cowboy_static, { priv_dir,  lpr_nso_site, "logo"       } },
+				{ "/css/[...]",     cowboy_static, { priv_dir,  lpr_nso_site, "css"        } },
+				{ "/library/[...]", cowboy_static, { priv_dir,  lpr_nso_site, "library"    } },
+				{ "/lib/[...]",     cowboy_static, { priv_dir,  lpr_nso_site, "lib"        } },
+				{ "/static/[...]",  cowboy_static, { priv_dir,  lpr_nso_site, "static"     } }
+			]
+		}
 	]),
 	
+	Domain = get_env( "DOMAIN", "nso.libertarian-party.ru" ),
+	
+	lager:info( "compiling redirector to HTTPS for ~p", [ Domain ] ),
 	Redir = cowboy_router:compile
 	([
-		{ '_', [
-			{ "/.well-known/[...]", cowboy_static, { priv_dir, lpr_nso_site, ".well-known" } }, %nso.libertarian-party.ru/.well-known/pki-validation/D9B3842C92942DEE765B981127A6C8F7.txt
-			{ "/[...]", lpr_nso_site_redirector, [] }
-			%{ '_', lpr_nso_site_redirector, [] }
-		]}
+		{
+			'_',
+			[ { "/[...]", lpr_nso_site_redirector, [ list_to_binary( Domain ) ] } ]
+		}
 	]),
-	
-	% UNIT_ENV :== staging | production | test | development
 
-	Port =
-		case os:getenv( "UNIT_PORT" ) of
-			false  -> 8443;
-			Result -> list_to_integer( Result )
-		end,
-	
-	Libcat = filelib:is_regular( "/home/shar/.ssh/libcat.site.key" ),
-	Sibext = filelib:is_regular( "/home/shar/.ssh/sibext.com.key" ),
+	HTTP_port  = to_integer( get_env( "HTTP_PORT",  8080 ) ),
+	HTTPS_port = to_integer( get_env( "HTTPS_PORT", 8443 ) ),
 	
 	Conn_options =
 		[
-			{ cacertfile, "/home/shar/.ssh/nso.libertarian-party.ru.ca.crt" },
-			{ certfile,   "/home/shar/.ssh/nso.libertarian-party.ru.crt"    },
-			{ keyfile,    "/home/shar/.ssh/nso.libertarian-party.ru.key"    }
+			{ cacertfile, get_env( "CACERT_FILE", "/home/shar/.ssh/nso.libertarian-party.ru.ca.crt" ) },
+			{ certfile,   get_env( "CERT_FILE",   "/home/shar/.ssh/nso.libertarian-party.ru.crt"    ) },
+			{ keyfile,    get_env( "KEY_FILE",    "/home/shar/.ssh/nso.libertarian-party.ru.key"    ) }
 		],
 		
+	lager:info( "starting HTTP: ~p", [ HTTP_port ] ),
 	{ ok, _ } = cowboy:start_clear
 	(
-		http, %300,
-		[{ port, 8080 }],
+		http,
+		[{ port, HTTP_port }],
 		#{ env => #{ dispatch => Redir } }
 	),
 	
-	case Conn_options of
-	
-		[] ->
-
-			lager:info( "starting plain HTTP:~p", [ Port ] ),
-			{ ok, _ } = cowboy:start_clear
-			(
-				http, %300,
-				[{ port, Port }],
-				#{ env => #{ dispatch => Dispatch } }
-			);
-			
-		_ ->
-    
-			lager:info( "starting TLS:~p( ~p )", [ Port, Conn_options ] ),
-			{ ok, _ } = cowboy:start_tls
-			(
-				https, %300,
-				lists:append( [ [{ port, Port }], Conn_options, [{ verify, verify_none }] ] ),
-				%[ { env, [ { dispatch, Dispatch } ] } ]
-				#{ env => #{ dispatch => Dispatch } }
-			)
-	end,
+	lager:info( "starting HTTPS: ~p ( ~p )", [ HTTPS_port, Conn_options ] ),
+	{ ok, _ } = cowboy:start_tls
+	(
+		https,
+		lists:append( [ [{ port, HTTPS_port }], Conn_options, [{ verify, verify_none }] ] ),
+		#{ env => #{ dispatch => Dispatch } }
+	),
 	
 	lpr_nso_site_sup:start_link()
 .
